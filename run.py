0320f436077ca246082ac6ebe0f3276824dda498
TeamTBD-Stevens <62127144+TeamTBD-Stevens@users.noreply.github.com>
2020-04-03 01:53:51 -0400

    Add files via upload

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 import sys
 import time
 import shift
+import datetime
 
 trader = shift.Trader("tbd")
 try:
@@ -10,13 +11,6 @@ except shift.IncorrectPasswordError as e:
     print(e)
 except shift.ConnectionTimeoutError as e:
     print(e)
-
-
-limit_buy = shift.Order(shift.Order.Type.LIMIT_BUY, "VIXY", 60, 35.00)
-trader.submit_order(limit_buy)
-
-limit_buy = shift.Order(shift.Order.Type.LIMIT_BUY, "AAPL", 1, 170.00)
-trader.submit_order(limit_buy)
 
 print("Buying Power\tTotal Shares\tTotal P&L\tTimestamp")
 print(
@@ -29,4 +23,46 @@ print(
     )
 )
 
-trader.disconnect()
+
+tickets = ['MCD', 'NKE', 'PG', 'WMT', 'AAPL', 'CSCO', 'MSFT', 'IBM']
+rangePERCENT = [0.0126, 0.0159, 0.0133, 0.0135, 0.0174, 0.0167, 0.015, 0.014]
+openPrice = []
+increment = [0, 0.0001, 0.0005, 0.001, 0.0015, 0.002, 0.003, 0.04, 0.005, 0.07]
+for i in range(8):
+    a = trader.get_best_price(tickets[i])
+
+    openPrice.append((a.get_bid_price() + a.get_ask_price())/ 2)
+
+    limit_buy = shift.Order(shift.Order.Type.LIMIT_BUY, tickets[i], 1, openPrice[0]+0.01)
+    trader.submit_order(limit_buy)
+
+    for k in increment:
+        limit_buy = shift.Order(shift.Order.Type.LIMIT_BUY, tickets[i], 1, openPrice[i]*(1-rangePERCENT[i]/2-k))
+        trader.submit_order(limit_buy)
+
+        limit_sell = shift.Order(shift.Order.Type.LIMIT_SELL, tickets[i], 1, openPrice[i]*(1+rangePERCENT[i]/2+k))
+        trader.submit_order(limit_sell)
+
+
+print(
+    "Symbol\t\t\t\tType\t  Price\t\tSize\tExecuted\tID\t\t\t\t\t\t\t\t\t\t\t\t\t\t Status\t\tTimestamp"
+)
+for order in trader.get_submitted_orders():
+    if order.status == shift.Order.Status.FILLED:
+        price = order.executed_price
+    else:
+        price = order.price
+    print(
+        "%6s\t%16s\t%7.2f\t\t%4d\t\t%4d\t%36s\t%23s\t\t%26s"
+        % (
+            order.symbol,
+            order.type,
+            price,
+            order.size,
+            order.executed_size,
+            order.id,
+            order.status,
+            order.timestamp,
+        )
+    )
+
